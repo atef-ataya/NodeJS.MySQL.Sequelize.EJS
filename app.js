9fee6cb552323f647c894bc41b2e427b23c03cb3
atefataya81 <atef.ataya@gmail.com>
2021-11-25 21:36:46 +0400

    Adding Body Parser for Incoming Request

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const http = require('http');
+//Core Module Specific Modules
+// const http = require('http');
 
+//Third Party Packages
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express(); //request handler
 
-const server = http.createServer(app);
+//Middlewares
+app.use(bodyParser.urlencoded({ extended: true })); //middleware body parser url-encoded bodies and at the end it will execute next()
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+//Server Configuration
+app.listen(3000);
