691b215961a0585cd4fc5e71e86bdd2ea7a1823b
atefataya81 <atef.ataya@gmail.com>
2021-11-28 21:37:17 +0400

    Enhancing the App

--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,19 @@ const path = require('path');
 //Third Party Module
 const express = require('express');
 //My Own Imports
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+// / => GET
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
